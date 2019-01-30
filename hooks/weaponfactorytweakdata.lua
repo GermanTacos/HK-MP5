c1e5d644c56c650af4d28fb188f2408e1e338b51
GermanTacos <jakebuilds@gmail.com>
2019-01-29 23:07:27 -0500

    new firing sounds + more old animation work

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -8,6 +8,29 @@ Hooks:PostHook( WeaponFactoryTweakData, "init", "MP5ModInit", function(self)
 		
 		self.parts.wpn_fps_smg_slap_10_m_straight_30.unit = "units/mods/weapons/wpn_fps_smg_slap_10_mag_pts/wpn_fps_smg_slap_10_m_straight_30_old"
 		self.parts.wpn_fps_smg_slap_40_m_straight_30.unit = "units/mods/weapons/wpn_fps_smg_slap_40_mag_pts/wpn_fps_smg_slap_40_m_straight_30_old"
+	
+		self.parts.wpn_fps_smg_slap_919_m_curve_30.animations = {
+			reload_not_empty = "reload_not_empty",
+			reload = "reload"
+		}
+		self.parts.wpn_fps_smg_slap_919_m_curve_15.animations = {
+			reload_not_empty = "reload_not_empty",
+			reload = "reload"
+		}
+		self.parts.wpn_fps_smg_slap_919_m_waffle_30.animations = {
+			reload_not_empty = "reload_not_empty",
+			reload = "reload"
+		}
+		self.parts.wpn_fps_smg_slap_919_m_waffle_15.animations = {
+			reload_not_empty = "reload_not_empty",
+			reload = "reload"
+		}
+		
+		self.slap_919.pd2_anims_only = true
+		
+		self.slap_919.timers.reload_not_empty = 2.4
+		self.slap_919.timers.reload_empty = 3.6
+		
 	end
 	
 end )
