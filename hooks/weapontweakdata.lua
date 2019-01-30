52980e6bcf81eca6805c353d1c0bc2c36fa1f1fc
IKillerwolfI <killerwolf.no1@gmail.com>
2019-01-30 05:02:19 +0100

    stuff

--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -5,13 +5,13 @@ if ( self.slap_919 ) then
 	self:SetupAttachmentPoint("slap_919", {
 		name = "a_ns", 
         base_a_obj = "a_ns", 
-        position = Vector3( 0, 2, 0 ), -- -16
+        position = Vector3( 0, -0.8, -0.2 ), -- -16
         rotation = Rotation( 0, 0, 0 ) 
 	})
 	self:SetupAttachmentPoint("slap_919", {
 		name = "a_fl", 
         base_a_obj = "a_fl", 
-        position = Vector3( 0, 2, 0 ), 
+        position = Vector3( 0.5, 1, 0 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
 	self:SetupAttachmentPoint("slap_919", {
