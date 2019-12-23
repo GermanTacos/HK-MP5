38c3d4583446c84e8be48184d3122a72ec978409
IKillerwolfI <killerwolf.no1@gmail.com>
2019-12-23 18:08:39 +0100

    Stance/Barrel
    -Added dummy barrel
    -Added stance stuff for sights and the default irons and shit

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -1,5 +1,30 @@
 Hooks:PostHook( WeaponFactoryTweakData, "init", "MP5ModInit", function(self)
 
+local vanilla_mp5sight = {
+	"wpn_fps_upg_o_specter",
+	"wpn_fps_upg_o_aimpoint",
+	"wpn_fps_upg_o_aimpoint_2",
+	"wpn_fps_upg_o_docter",
+	"wpn_fps_upg_o_eotech",
+	"wpn_fps_upg_o_t1micro",
+	"wpn_fps_upg_o_cmore",
+	"wpn_fps_upg_o_acog",
+	"wpn_fps_upg_o_cs",
+	"wpn_fps_upg_o_eotech_xps",
+	"wpn_fps_upg_o_reflex",
+	"wpn_fps_upg_o_rx01",
+	"wpn_fps_upg_o_rx30",
+	"wpn_fps_upg_o_spot"
+}
+
+--- Gun ---
+for id, o_id in pairs(vanilla_mp5sight) do
+	self.wpn_fps_smg_slap_919.adds[o_id] = {"wpn_fps_smg_slap_919_sm_claw"}
+	self.wpn_fps_smg_slap_40.adds[o_id] = {"wpn_fps_smg_slap_919_sm_claw"}
+	self.wpn_fps_smg_slap_10.adds[o_id] = {"wpn_fps_smg_slap_919_sm_claw"}
+end
+
+
 	if BeardLib.Utils:FindMod("Old MP5 Animations") then
 		
 		self.wpn_fps_smg_slap_919.animations = nil
--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -7,7 +7,7 @@ if BeardLib.Utils:FindMod("Old MP5 Animations") then
 	self.slap_919.use_stance = "slap_919_oldads"
 end
 
-if ( self.slap_919 ) then
+if self.SetupAttachmentPoint then
 --- Base stuff ---	
 	self:SetupAttachmentPoint("slap_919", {
 		name = "a_ns", 
@@ -30,10 +30,72 @@ if ( self.slap_919 ) then
 	self:SetupAttachmentPoint("slap_919", {
 		name = "a_o", 
         base_a_obj = "a_o", 
-        position = Vector3( 0, 0, 0 ), 
+        position = Vector3( 0, -2, 0.8 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
 	self:SetupAttachmentPoint("slap_919", {
+		name = "a_os", 
+        base_a_obj = "a_os", 
+        position = Vector3( 0, 0, 0 ), 
+        rotation = Rotation( 0, 0, 0 )
+	})
+--- Base stuff ---	
+	self:SetupAttachmentPoint("slap_10", {
+		name = "a_ns", 
+        base_a_obj = "a_ns", 
+        position = Vector3( 0, -0.8, -0.2 ), -- -16
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("slap_10", {
+		name = "a_fl", 
+        base_a_obj = "a_fl", 
+        position = Vector3( 0.5, 1, 0 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("slap_10", {
+		name = "a_gl", 
+        base_a_obj = "a_fl", 
+        position = Vector3( -2.88, 27.2, -3.1 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("slap_10", {
+		name = "a_o", 
+        base_a_obj = "a_o", 
+        position = Vector3( 0, -2, 0.8 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("slap_10", {
+		name = "a_os", 
+        base_a_obj = "a_os", 
+        position = Vector3( 0, 0, 0 ), 
+        rotation = Rotation( 0, 0, 0 )
+	})
+--- Base stuff ---	
+	self:SetupAttachmentPoint("slap_40", {
+		name = "a_ns", 
+        base_a_obj = "a_ns", 
+        position = Vector3( 0, -0.8, -0.2 ), -- -16
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("slap_40", {
+		name = "a_fl", 
+        base_a_obj = "a_fl", 
+        position = Vector3( 0.5, 1, 0 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("slap_40", {
+		name = "a_gl", 
+        base_a_obj = "a_fl", 
+        position = Vector3( -2.88, 27.2, -3.1 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("slap_40", {
+		name = "a_o", 
+        base_a_obj = "a_o", 
+        position = Vector3( 0, -2, 0.8 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("slap_40", {
 		name = "a_os", 
         base_a_obj = "a_os", 
         position = Vector3( 0, 0, 0 ), 
