be6b2d9d1669b0cd53abf7e4896c1959d2f4f5ca
GermanTacos <jakebuilds@gmail.com>
2019-12-24 12:10:02 -0500

    Added auto update and commented out some code

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -32,36 +32,36 @@ for id, o_id in pairs(vanilla_mp5sight) do
 end
 
 
-	if BeardLib.Utils:FindMod("Old MP5 Animations") then
+	-- if BeardLib.Utils:FindMod("Old MP5 Animations") then
 		
-		self.wpn_fps_smg_slap_919.animations = nil
+		-- self.wpn_fps_smg_slap_919.animations = nil
 	
-		self.parts.wpn_fps_smg_slap_919_m_curve_30.unit = "units/mods/weapons/wpn_fps_smg_slap_919_mag_pts/wpn_fps_smg_slap_919_m_curve_30_old"
-		self.parts.wpn_fps_smg_slap_919_m_curve_15.unit = "units/mods/weapons/wpn_fps_smg_slap_919_mag_pts/wpn_fps_smg_slap_919_m_curve_15_old"
-		self.parts.wpn_fps_smg_slap_919_m_waffle_30.unit = "units/mods/weapons/wpn_fps_smg_slap_919_mag_pts/wpn_fps_smg_slap_919_m_waffle_30_old"
-		self.parts.wpn_fps_smg_slap_919_m_waffle_15.unit = "units/mods/weapons/wpn_fps_smg_slap_919_mag_pts/wpn_fps_smg_slap_919_m_waffle_15_old"
+		-- self.parts.wpn_fps_smg_slap_919_m_curve_30.unit = "units/mods/weapons/wpn_fps_smg_slap_919_mag_pts/wpn_fps_smg_slap_919_m_curve_30_old"
+		-- self.parts.wpn_fps_smg_slap_919_m_curve_15.unit = "units/mods/weapons/wpn_fps_smg_slap_919_mag_pts/wpn_fps_smg_slap_919_m_curve_15_old"
+		-- self.parts.wpn_fps_smg_slap_919_m_waffle_30.unit = "units/mods/weapons/wpn_fps_smg_slap_919_mag_pts/wpn_fps_smg_slap_919_m_waffle_30_old"
+		-- self.parts.wpn_fps_smg_slap_919_m_waffle_15.unit = "units/mods/weapons/wpn_fps_smg_slap_919_mag_pts/wpn_fps_smg_slap_919_m_waffle_15_old"
 		
-		--self.parts.wpn_fps_smg_slap_10_m_straight_30.unit = "units/mods/weapons/wpn_fps_smg_slap_10_mag_pts/wpn_fps_smg_slap_10_m_straight_30_old"
-		--self.parts.wpn_fps_smg_slap_40_m_straight_30.unit = "units/mods/weapons/wpn_fps_smg_slap_40_mag_pts/wpn_fps_smg_slap_40_m_straight_30_old"
+		-- self.parts.wpn_fps_smg_slap_10_m_straight_30.unit = "units/mods/weapons/wpn_fps_smg_slap_10_mag_pts/wpn_fps_smg_slap_10_m_straight_30_old"
+		-- self.parts.wpn_fps_smg_slap_40_m_straight_30.unit = "units/mods/weapons/wpn_fps_smg_slap_40_mag_pts/wpn_fps_smg_slap_40_m_straight_30_old"
 	
-		self.parts.wpn_fps_smg_slap_919_m_curve_30.animations = {
-			reload_not_empty = "reload_not_empty",
-			reload = "reload"
-		}
-		self.parts.wpn_fps_smg_slap_919_m_curve_15.animations = {
-			reload_not_empty = "reload_not_empty",
-			reload = "reload"
-		}
-		self.parts.wpn_fps_smg_slap_919_m_waffle_30.animations = {
-			reload_not_empty = "reload_not_empty",
-			reload = "reload"
-		}
-		self.parts.wpn_fps_smg_slap_919_m_waffle_15.animations = {
-			reload_not_empty = "reload_not_empty",
-			reload = "reload"
-		}
+		-- self.parts.wpn_fps_smg_slap_919_m_curve_30.animations = {
+			-- reload_not_empty = "reload_not_empty",
+			-- reload = "reload"
+		-- }
+		-- self.parts.wpn_fps_smg_slap_919_m_curve_15.animations = {
+			-- reload_not_empty = "reload_not_empty",
+			-- reload = "reload"
+		-- }
+		-- self.parts.wpn_fps_smg_slap_919_m_waffle_30.animations = {
+			-- reload_not_empty = "reload_not_empty",
+			-- reload = "reload"
+		-- }
+		-- self.parts.wpn_fps_smg_slap_919_m_waffle_15.animations = {
+			-- reload_not_empty = "reload_not_empty",
+			-- reload = "reload"
+		-- }
 		
-	end
+	-- end
 	
 	for id, fl_id in pairs(vanilla_mp5_fl) do
 		self.parts.wpn_fps_smg_slap_919_hg_ris.override[fl_id] = {a_obj="a_fl_ris"}
